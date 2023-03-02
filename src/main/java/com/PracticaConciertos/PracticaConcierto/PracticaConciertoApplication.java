4446339f4967bac2637405092fc0a47627a8634a
MpD <mariapauladelvalle4@gmail.com>
2023-03-01 19:46:36 -0600

    Primer commit - Creacion proyecto

---
+++ src/main/java/com/PracticaConciertos/PracticaConcierto/PracticaConciertoApplication.java
@@ -0,0 +1,13 @@
+package com.PracticaConciertos.PracticaConcierto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaConciertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaConciertoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PracticaConciertos/PracticaConcierto/PracticaConciertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PracticaConciertos.PracticaConcierto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaConciertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
